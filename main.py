5cccc21253344e0eb0f38a38a8e8c61c4e11e830
Abilash Sivasith <abi.sivasith.world@gmail.com>
2024-03-10 20:46:34 +1300

    created main file

---
+++ main.py
@@ -0,0 +1,32 @@
+"""
+Reads books data and gives graphs with usefull infomation
+
+Created By:
+Abilash Sivasith
+"""
+
+EXCEL_FILENAME = ""
+
+def open_excel():
+    """opens the excell file"""
+    pass
+
+def workable_file():
+    """turns the provided excell file into a worksable format"""
+    pass
+
+def graph_per_genre():
+    """graph books based on main genre"""
+    pass
+
+def graph_per_genre_read():
+    """graph read books based on main genre"""
+    pass
+
+def graph_per_genre_unread():
+    """graph unread books based on main genre"""
+    pass
+
+def main():
+    """main loop with what data you want to display"""
+    pass
