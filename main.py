dcc762f1282e9ea70f27e5f8c78c388dd8e6602e
Abilash Sivasith <abi.sivasith.world@gmail.com>
2024-03-11 22:47:04 +1300

    completted workable_file_function

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Abilash Sivasith
 """
 
 import pandas as pd
+from pprint import pprint
 
 EXCEL_FILENAME = "books_I_want_to_read.xlsx"
 
@@ -21,9 +22,22 @@ def open_excel():
 def workable_file():
     """turns the provided excell file into a worksable format"""
     excel_file_to_read = open_excel()
-    
-    for col_tittle in excel_file_to_read:
-        print(col_tittle)
+    book_info_list = []
+    i = 0 
+    for index, row in excel_file_to_read.iterrows():
+        book_name = row['Tittle']
+        book_authors = (row['Author 1'], row['Author 2'])
+        book_genre = (row['Genre 1'], row['Genre 2'])
+        book_read = row['Read']
+        book_owned = row['Owned']
+        book_year_read = row['Year Read']
+        tuple_of_book_info = (book_name, book_authors, book_genre, book_read, book_owned, book_year_read)
+        book_info_list.append(tuple_of_book_info)
+    pprint(book_info_list)
+        
+
+            
+            
     
     
     '''book_title = excel_file_to_read['Tittle']
